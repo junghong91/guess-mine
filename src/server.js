cc9ceb14a0ad68399b76f00a3e0230930b0da381
Jung Hong <junghong0512@gmail.com>
2020-01-07 15:47:38 +0900

    To Do: Pug(views), finished

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
+import { join } from "path";
 import express from "express";
 
 const PORT = 3000;
 const app = express();
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+
+app.get("/", (req, res) => res.render("home"));
 
 const handleListening = () => {
   console.log(`Server Running: http://localhost:${PORT}`);
