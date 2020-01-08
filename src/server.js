7e8f04e749c117076b38706120caebe2155b19f8
Jung Hong <junghong0512@gmail.com>
2020-01-08 15:35:11 +0900

    Exercising for Chatting with SOcketIO

--- src/server.js
+++ src/server.js
@@ -22,17 +22,26 @@ const handleListening = () => {
 const server = app.listen(PORT, handleListening); // trafic 이 다르기 때문에, 같은 포트에서 작업한다.
 
 // socketIO --> server 와 client 가 동시에 될 수 있다.
-// 위에서 만든 server 를 socketIO에 전달하는 과정. ('io' listen all the event)
+// 위에서 만든 server 를 socketIO에 전달하는 과정. ('io': name of server, listen all the event from the client)
 const io = socketIO.listen(server); // localhost:3000/socket.io/socket.io.js  <- To make socketIO frontend와 socketIO backend communicate each other
 
-let sockets = [];
+// let sockets = [];
 
 // connection 의 시작점(entry point)
 io.on("connection", socket => {
   // sockets.push(socket.id);
   // setTimeout(() => socket.emit("hello"), 5000); // server 가 event를 emit 한다.
-  // setTimeout(() => socket.broadcast.emit("hello"), 5000); // broadcast 는 첫번째로 접속한 client가 제외된다.
-  socket.on("helloGuys", () => console.log("The client said hello"));
+  // setTimeout(() => socket.broadcast.emit("hello"), 5000); // broadcast 현재 접속한 client가 제외된다.
+  socket.on("newMessage", ({ message }) => {
+    // console.log(message);
+    socket.broadcast.emit("messageNotif", {
+      message,
+      nickname: socket.nickname || "Anonymous"
+    }); // message와 nickname 은 socket 객체에 들어있다.
+  });
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
 });
 
 // setInterval(() => console.log(sockets), 1000); // socket이 어떻게 생겼는지...
--- src/static/index.js
+++ src/static/index.js
@@ -1,5 +1,21 @@
 const socket = io("/");
 
-socket.on("hello", () => console.log("Somebody join")); // hello 라는 이벤트가 server로 부터 발생하면, console.log()
+// socket.on("hello", () => console.log("Somebody join")); // hello 라는 이벤트가 server로 부터 발생하면, console.log()
 
-setTimeout(() => socket.emit("helloGuys"), 4000);
+// setTimeout(() => socket.emit("helloGuys"), 4000);
+
+function sendMessage(message) {
+  socket.emit("newMessage", { message }); // { message: message }
+  console.log(`You: ${message}`);
+}
+
+function setNickname(nickname) {
+  socket.emit("setNickname", { nickname });
+}
+
+function handleMessageNotif(data) {
+  const { message, nickname } = data;
+  console.log(`${nickname}: ${message}`);
+}
+
+socket.on("messageNotif", handleMessageNotif);
